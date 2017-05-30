da00e92040652d01595a3fb501e169aa1c50e9d4
Seun Martins <seundotme@gmail.com>
2017-05-30 23:26:45 +0100

    Minor fixes + refactor on live mode

--- schemas.js
+++ schemas.js
@@ -3,7 +3,6 @@
 const Joi = require('joi');
 
 const base = {
-  'liveMode': Joi.string().empty('').default('y').valid(['y', 'n']),
   'PBFPubKey': Joi.string().required(),
   'amount': Joi.number().allow(''),
   'customer_email': Joi.string().email().allow(''),
@@ -12,7 +11,8 @@ const base = {
   'custom_title': Joi.string().allow(''),
   'custom_description': Joi.string().allow(''),
   'redirect_url': Joi.string().uri().empty('').default(''),
-  'custom_logo': Joi.string().uri().empty('').default('')
+  'custom_logo': Joi.string().uri().empty('').default(''),
+  'liveMode': Joi.string() // not saved intentionally since it's only used on "npm start"
 };
 
 const dynamicFields = {
